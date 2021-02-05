6bb6aea4ab8e3ff364870ce7fa0129f24ee9cf8b
Thai-Ngoc <ngoclptSE00512x@funix.edu.vn>
2021-02-05 07:59:44 +0700

    push

---
+++ PRM391x_ShopMovies_ngoclptSE00512x/app/src/main/java/com/example/prm391x_shopmovies_ngoclptse00512x/Adapter.java
@@ -0,0 +1,6 @@
+package com.example.prm391x_shopmovies_ngoclptse00512x;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ItemViewHolder> {
+}
---
+++ PRM391x_ShopMovies_ngoclptSE00512x/app/src/main/java/com/example/prm391x_shopmovies_ngoclptse00512x/Model.java
@@ -0,0 +1,7 @@
+package com.example.prm391x_shopmovies_ngoclptse00512x;
+
+public class Model {
+    public String image_url;
+    public String title;
+    public String price;
+}
