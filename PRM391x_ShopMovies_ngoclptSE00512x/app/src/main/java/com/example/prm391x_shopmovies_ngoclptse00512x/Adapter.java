25d7a3e92ddee2e2feda1aa5038e31e7e3d29508
Thai-Ngoc <ngoclptSE00512x@funix.edu.vn>
2021-02-16 17:02:40 +0700

    16/02

--- PRM391x_ShopMovies_ngoclptSE00512x/app/src/main/java/com/example/prm391x_shopmovies_ngoclptse00512x/Adapter.java
+++ PRM391x_ShopMovies_ngoclptSE00512x/app/src/main/java/com/example/prm391x_shopmovies_ngoclptse00512x/Adapter.java
@@ -1,6 +1,44 @@
 package com.example.prm391x_shopmovies_ngoclptse00512x;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ItemViewHolder> {
+
+    private List<Model> item;
+    private Context context;
+
+    public Adapter(List<Model> item, Context context) {
+        this.item = item;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return item.size();
+    }
+
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+    }
 }
